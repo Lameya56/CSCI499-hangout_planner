3514137e3185b1c960cf7a944156fe156e6a57cf
farhansofian290 <123140133+farhansofian290@users.noreply.github.com>
2025-11-12 19:37:06 -0500

    ec2psql update

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,18 @@
-import pg from 'pg'
+import pg from 'pg';
 import dotenv from 'dotenv';
-dotenv.config({path: '../.env'});
+
+dotenv.config({ path: '../.env' });
+
 const config = {
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-}
-export const pool = new pg.Pool(config)
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false, // allows self-signed AWS certificates
+  },
+};
+
+export const pool = new pg.Pool(config);
