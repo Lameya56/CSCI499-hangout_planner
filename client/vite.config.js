a787f00a536409c22b22ce9541b517de6b4bc955
farhansofian290 <farhansofian29@gmail.com>
2025-11-26 03:41:58 +0000

    ec2working

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,14 +18,14 @@ export default defineConfig({
     proxy: {
       "/api": {
         // ⚠️ Change localhost → EC2 public IP
-        target: "http://35.92.203.139:3001/",
+        target: "http://lets-go.site/",
         changeOrigin: true,
       },
     },
     host: "0.0.0.0", // allows all incoming connections
     port: 5173,       // you can leave this or change if needed
     allowedHosts: [
-      "35.92.203.139",   // ✅ your EC2 instance public IP
+      "lets-go.site",   // ✅ your EC2 instance public IP
       "localhost",
     ],
   },
