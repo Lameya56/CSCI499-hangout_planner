a0bc8ce145f44c08027963802ed59f352d2fcebd
farhansofian290 <123140133+farhansofian290@users.noreply.github.com>
2025-12-13 21:20:21 -0500

    imagesTest

--- server/server.js
+++ server/server.js
@@ -13,30 +13,21 @@ import invitationRoutes from './routes/invitationRoutes.js';
 import voteRoutes from './routes/voteRoutes.js';
 import './utils/cronJob.js';
 import groupRoutes from './routes/groupRoutes.js';
-import exploreRoutes from './routes/exploreRoutes.js';
+import imagesRoutes from './routes/images.js';
+import path from "path";
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 const server = createServer(app);
-const isLocal = process.env.LOCAL === "TRUE";
-
-const origins = isLocal
-  ? ["http://localhost:5173"] // local dev frontend
-  : [
-      "http://lets-go.site",
-      "https://lets-go.site",
-      "http://www.lets-go.site",
-      "https://www.lets-go.site"
-    ];
- 
+const origins = ["http://lets-go.site", "https://lets-go.site", "http://www.lets-go.site", "https://www.lets-go.site"];
 const io = new Server(server, {
   cors: {
     origin: origins,
     methods: ["GET", "POST"],
   },
 });
-
-
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
@@ -48,27 +39,9 @@ io.on("connection", (socket) => {
 
 
 
-  socket.on("chat message", async ({groupID, msg, senderID}) => {
+  socket.on("chat message", ({groupID, msg, sender}) => {
     console.log(`Message in group: ${groupID}`, msg);
-    try{
-      await pool.query(
-        `INSERT INTO chat (plan_id, user_id, message) VALUES ($1, $2, $3)`,
-        [groupID, senderID, msg]
-      );
-
-      const username = await pool.query(
-        `SELECT name FROM users WHERE id = $1`,
-        [senderID]
-      );
-
-      const sendersName = username.rows[0].name;
-
-      io.to(groupID).emit("chat message", {msg, senderID, sender: sendersName, time: new Date()})
-    }
-    catch(err){
-      console.log("---------message not sent to database---------")
-      console.log(err);
-    }
+    io.to(groupID).emit("chat message", {msg, sender})
   });
 
 
@@ -87,29 +60,28 @@ io.on("connection", (socket) => {
 
 app.use(cors({ origin: origins, credentials: true }));
 app.use(express.json())
+
+if (process.env.LOCAL === "TRUE") {
+  app.use('/uploads', express.static('uploads')); // <-- make local files accessible
+  }
+
 // Routes
 app.get('/', (req, res) => {
-  res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">Hangout Planner from server </h1>')
+  app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 })
 app.use("/api", authRoutes);
 app.use("/api/plans", planRoutes);
 app.use('/api/invitations', invitationRoutes);
 app.use('/api/votes', voteRoutes);
 app.use("/api/groups", groupRoutes);
-app.use('/api/explore', exploreRoutes);
+app.use('/api/images', imagesRoutes)
 
 const PORT = process.env.PORT || 3001
 
 
 
 server.listen(PORT, "0.0.0.0", () => {
-  if(process.env.LOCAL === "TRUE"){
-    console.log(`🚀 Server listening on http://localhost:${PORT}`)
-  }
-  else{
-    console.log(`🚀 Server listening on https://lets-go.site`)
-  }
-  
+  console.log(`🚀 Server listening on http://lets-go.site:${PORT}`)
 })
 console.log("JWT_SECRET is:", process.env.JWT_SECRET);
 
